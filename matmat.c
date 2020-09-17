16efd45c357eae9ec092d52d262b9f266b9518d0
Amanda Bienz <bienz@wheeler-sn.alliance.unm.edu>
2020-09-17 17:15:15 -0600

    Added tau trace

--- matmat.c
+++ matmat.c
@@ -15,16 +15,18 @@
 // Matrix-Matrix Multiplication of Doubles (Single Pointer)
 void matmat(int n, double* A, double* B, double* C, int n_iter)
 {
+    int i, j, k;
+    int iter;
     double val;
-    for (int iter = 0; iter < n_iter; iter++)
+    for (iter = 0; iter < n_iter; iter++)
     {
-#pragma omp parallel for private(val) schedule(runtime)
-        for (int i = 0; i < n; i++)
+#pragma omp parallel for private(val, i, j, k) schedule(runtime)
+        for (i = 0; i < n; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (j = 0; j < n; j++)
             {
                 val = A[i*n+j];
-                for (int k = 0; k < n; k++)
+                for (k = 0; k < n; k++)
                 {
                     C[i*n+k] = val * B[j*n+k];
                 }
@@ -36,10 +38,11 @@ void matmat(int n, double* A, double* B, double* C, int n_iter)
 
 double sum(int n, double* C)
 {
+    int i, j;
     double s = 0;
-    for (int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (j = 0; j < n; j++)
         {
             s += C[i*n+j];
         }
@@ -51,21 +54,22 @@ double sum(int n, double* C)
 // Test vectorization improvements for both doubles and floats
 int main(int argc, char* argv[])
 {
-
+    int i, j;
     double start, end;
     int n_access = 1000000000;
 
     int n = atoi(argv[1]);
     int n_iter = n_access / (n*n*n);
+    n_iter = 1;
 
     double* A = (double*)malloc(n*n*sizeof(double));
     double* B = (double*)malloc(n*n*sizeof(double));
     double* C = (double*)malloc(n*n*sizeof(double));
 
-    #pragma omp parallel for schedule(runtime)
-    for (int i = 0; i < n; i++)
+    #pragma omp parallel for schedule(runtime) private(i,j)
+    for (i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (j = 0; j < n; j++)
         {
             A[i*n+j] = 1.0/(i*n+j+1);
             B[i*n+j] = 1.0;
