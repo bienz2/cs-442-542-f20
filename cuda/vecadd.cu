eefb04597c3b6f663b8b5c71cb9b45197caa61c8
Amanda Bienz <bienz@xena.xena.alliance.unm.edu>
2020-11-09 10:09:43 -0700

    Added cuda examples

--- cuda/vecadd.cu
+++ cuda/vecadd.cu
@@ -1,4 +1,14 @@
 #include <cuda.h>
+#include "../timer.h"
+
+__global__
+void vecAddKernel(float* A, float* B, float* C, int n)
+{
+    int i = blockDim.x*blockIdx.x + threadIdx.x;
+    if (i < n) C[i] = A[i] + B[i];
+
+printf("i %d\n", i);
+}
 
 void vecAdd(float* h_A, float* h_B, float* h_C, int n)
 {
@@ -6,18 +16,66 @@ void vecAdd(float* h_A, float* h_B, float* h_C, int n)
         h_C[i] = h_A[i] + h_B[i];
 }
 
+double sum(int n, float* h_C)
+{
+    double s = 0;
+    for (int i = 0; i < n; i++)
+        s += h_C[i];
+    return s;
+}
+
 int main(int argc, char* argv[])
 {
     float *h_A, *h_B, *h_C;
+    double t0, tfinal;
     int n = atoi(argv[1]);
 
-    cudaMalloc((void**)&h_A, n*sizeof(float));
-    cudaMalloc((void**)&h_B, n*sizeof(float));
-    cudaMalloc((void**)&h_C, n*sizeof(float));
+    h_A = (float*)malloc(n*sizeof(float));
+    h_B = (float*)malloc(n*sizeof(float));
+    h_C = (float*)malloc(n*sizeof(float));
+    for (int i = 0; i < n; i++)
+    {
+        h_A[i] = 0.5;
+        h_B[i] = 0.2;
+    }
 
+    t0 = get_time();
     vecAdd(h_A, h_B, h_C, n);
+    tfinal = get_time() - t0;
+    printf("VecAdd Time %e, SumC %e\n", tfinal, sum(n, h_C));
 
-    cudaFree(h_A);
-    cudaFree(h_B);
-    cudaFree(h_C);
+
+    float *d_A, *d_B, *d_C;
+    
+    // Allocate device memory
+    cudaMalloc((void**)&d_A, n*sizeof(float));
+    cudaMalloc((void**)&d_B, n*sizeof(float));
+    cudaMalloc((void**)&d_C, n*sizeof(float));
+
+
+printf("h_A[0] %f\n", h_A[0]);
+    // Copy A and B to device memory
+    cudaMemcpy(d_A, h_A, n*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, n*sizeof(float), cudaMemcpyHostToDevice);
+
+    // Device vecAdd Kernel
+    t0 = get_time();
+    vecAddKernel<<<ceil(n/256.0), 256>>>(d_A, d_B, d_C, n);
+    tfinal = get_time() - t0;
+
+    // Copy C back to host memory
+printf("h_c[0] %f\n", h_C[0]);
+    cudaMemcpy(h_C, d_C, n*sizeof(float), cudaMemcpyDeviceToHost);
+
+printf("h_C[0] %f\n", h_C[0]);
+    printf("VecAddKernel Time %e, SumC %e\n", tfinal, sum(n, h_C));
+
+    // Free device memory
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
+
+    free(h_A);
+    free(h_B);
+    free(h_C);
 }
