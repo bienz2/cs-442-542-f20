3b754b362bf45c8fef4776fe674a5ac42e119f8d
Amanda Bienz <abienz@Amandas-MacBook-Pro.local>
2020-11-30 09:01:55 -0700

    Updated matmat heterogenenous

--- heterogenenous/matmat.cu
+++ heterogenenous/matmat.cu
@@ -5,8 +5,9 @@
 #include <math.h>
 #include <cuda.h>
 
-__global__ 
-void matrixMultKernel(double* A, double* B, double* C, int n)
+
+// Cuda Device Matrix Multiplication
+__global__ void matrixMultKernel(float* A, float* B, float* C, int n)
 {
     int col = blockIdx.x*blockDim.x + threadIdx.x;
     int row = blockIdx.y*blockDim.y + threadIdx.y;
@@ -20,6 +21,27 @@ void matrixMultKernel(double* A, double* B, double* C, int n)
     }
 }
 
+// Serial matrix-matrix multiplication
+void matmat(int n, double* A, double* B, double* C)
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int i, j, k;
+
+    double val;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            val = A[i*n+j];
+            for (k = 0; k < n; k++)
+            {
+                C[i*n+k] += val * B[j*n+k];
+            }
+        }
+    }
+ 
+}
 
 // Shift A 'rank_row' columns
 // Shift B 'rank_col' rows
@@ -32,21 +54,8 @@ void cannon(int n, double* A, double* B, double** C_ptr)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-    int num_gpus;
-    cudaGetDeviceCount(&num_gpus);
-    
-    MPI_Comm node_comm;
-    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, rank, 
-            MPI_INFO_NULL, &node_comm);
-    int ppn, node_rank;
-    MPI_Comm_rank(node_comm, &node_rank);
-    MPI_Comm_size(node_comm, &ppn);
-
-    int procs_per_gpu = ppn / num_gpus;
-    int gpu = node_rank / procs_per_gpu;
-    cudaSetDevice(gpu);
-
     int i, j;
+    int size = n*n*sizeof(double);
 
     double* C = (double*)malloc(n*n*sizeof(double));
     for (i = 0; i < n; i++)
@@ -57,30 +66,23 @@ void cannon(int n, double* A, double* B, double** C_ptr)
         }
     }
 
-    double *d_A, *d_B, *d_C;
-    cudaMalloc((void**)&d_A, n*n*sizeof(double));
-    cudaMalloc((void**)&d_B, n*n*sizeof(double));
-    cudaMalloc((void**)&d_C, n*n*sizeof(double));
-  
-    double *A2, *A3, *B2, *B3;
-    cudaMalloc((void**)&A2, n*n*sizeof(double));
-    cudaMalloc((void**)&A3, n*n*sizeof(double));
-    cudaMalloc((void**)&B2, n*n*sizeof(double));
-    cudaMalloc((void**)&B3, n*n*sizeof(double));
-    
-    cudaMemcpy(d_A, A, n*n*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, n*n*sizeof(double), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_C, C, n*n*sizeof(double), cudaMemcpyHostToDevice);
+    // Define other matrices to hold A and B
+    double* A2 = (double*)malloc(size);
+    double* B2 = (double*)malloc(size);
+    double* A3 = (double*)malloc(size);
+    double* B3 = (double*)malloc(size);
 
-    double* send_A = d_A;
-    double* send_B = d_B;
+    double *d_A, *d_B, *d_C;
+    cudaMalloc((void**)&d_A, size);
+    cudaMalloc((void**)&d_B, size);
+    cudaMalloc((void**)&d_C, size);
+    cudaMemcpy(d_C, C, size, cudaMemcpyHostToDevice);
+
+    double* send_A = A;
+    double* send_B = B;
     double* recv_A = A2;
     double* recv_B = B2;
     double* tmp;
-
-    dim3 dimBlock(32,32);
-    int grid_dim = ceil(n / 32.0);
-    dim3 dimGrid(grid_dim, grid_dim);
 
     int sq_num_procs = sqrt(num_procs);
     int rank_row = rank / sq_num_procs;
@@ -90,6 +92,10 @@ void cannon(int n, double* A, double* B, double** C_ptr)
     int proc_row, proc_col;
     int tag_a = 1234;
     int tag_b = 4321;
+
+    dim3 dimBlock(32,32);
+    int grid_dim = ceil(n / 32.0);
+    dim3 dimGrid(grid_dim, grid_dim);
 
     MPI_Request send_req_a, send_req_b, recv_req_a, recv_req_b;
 
@@ -111,6 +117,8 @@ void cannon(int n, double* A, double* B, double** C_ptr)
     // Send A 
     shift = rank_row;
     proc_col = rank_col + shift;
+    if (proc_col >= sq_num_procs) proc_col -= sq_num_procs;
+    proc = rank_row * sq_num_procs + proc_col;
     MPI_Isend(send_A, n*n, MPI_DOUBLE, proc, tag_a, MPI_COMM_WORLD, &send_req_a);
 
     // Send B
@@ -129,8 +137,10 @@ void cannon(int n, double* A, double* B, double** C_ptr)
     tag_b++;
 
     // After initial shift, can multiply pairs of matrices
-    matrixMultKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, n);
-
+    cudaMemcpy(d_A, recv_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, recv_B, size, cudaMemcpyHostToDevice);
+    matrixMultKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, n);    
+    //matmat(n, recv_A, recv_B, C);
 
     recv_A = A3;
     recv_B = B3;
@@ -170,8 +180,10 @@ void cannon(int n, double* A, double* B, double** C_ptr)
         MPI_Wait(&recv_req_b, MPI_STATUS_IGNORE);
 
         // After each step of communication, multiply locally recvd submatrices
-        matrixMultKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, n);
-
+	cudaMemcpy(d_A, recv_A, size, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_B, recv_B, size, cudaMemcpyHostToDevice);
+        matrixMultKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, n);    
+        //matmat(n, recv_A, recv_B, C);
 
         tag_a++;
         tag_b++;
@@ -189,8 +201,6 @@ void cannon(int n, double* A, double* B, double** C_ptr)
     free(recv_B);
     free(send_A);
     free(send_B);
-
-    cudaMemcpy(C, d_C, n*n*sizeof(double), cudaMemcpyDeviceToHost);
 
     *C_ptr = C;
 }
@@ -219,6 +229,7 @@ int main(int argc, char* argv[])
     int N = atoi(argv[1]);
     int sq_num_procs = sqrt(num_procs);
     int rank_row = rank / sq_num_procs;
+    int first_rank_row = rank_row*sq_num_procs;
     int rank_col = rank % sq_num_procs;
 
     int n = N / sq_num_procs;
@@ -227,6 +238,8 @@ int main(int argc, char* argv[])
     double* C;
 
     srand(rank*time(NULL));
+    int first_i = rank_row*N;
+    int first_j = rank_col;
     int i, j;
     for (i = 0; i < n; i++)
     {
@@ -239,7 +252,6 @@ int main(int argc, char* argv[])
     
     double sum_C, total_sum_C;
     double start, end;
-
 
     // Time Cannon's Method/
     MPI_Barrier(MPI_COMM_WORLD);
